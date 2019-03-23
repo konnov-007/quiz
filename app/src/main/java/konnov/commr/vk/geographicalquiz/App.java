cafc46967e6cc0d2418dcbaeab9bcdbb9991d6c9
Konnov Ilya <konnov-007@mail.ru>
2019-03-23 14:17:06 +0700

    Add insert sql statements

--- app/src/main/java/konnov/commr/vk/geographicalquiz/App.java
+++ app/src/main/java/konnov/commr/vk/geographicalquiz/App.java
@@ -3,12 +3,14 @@ package konnov.commr.vk.geographicalquiz;
 import android.app.Application;
 
 import konnov.commr.vk.geographicalquiz.data.DataStorage;
+import konnov.commr.vk.geographicalquiz.data.LocalDatabase;
 import konnov.commr.vk.geographicalquiz.data.UserRepository;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        LocalDatabase.getInstance(this);
         DataStorage.getInstance();
         UserRepository.getInstance();
     }
--- app/src/main/java/konnov/commr/vk/geographicalquiz/data/LocalDatabase.java
+++ app/src/main/java/konnov/commr/vk/geographicalquiz/data/LocalDatabase.java
@@ -1,8 +1,15 @@
 package konnov.commr.vk.geographicalquiz.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.HashMap;
+
+import konnov.commr.vk.geographicalquiz.objects.Question;
+import konnov.commr.vk.geographicalquiz.objects.Translation;
 
 
 public class LocalDatabase extends SQLiteOpenHelper {
@@ -27,6 +34,7 @@ public class LocalDatabase extends SQLiteOpenHelper {
 
     private Context mContext;
     private static LocalDatabase mInstance = null;
+    private SQLiteDatabase db;
 
     public static LocalDatabase getInstance(Context context) {
         if (mInstance == null) {
@@ -39,11 +47,13 @@ public class LocalDatabase extends SQLiteOpenHelper {
     private LocalDatabase(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
         mContext = context;
+        db = getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        createTables(sqLiteDatabase);
+        db = sqLiteDatabase;
+        createTables();
     }
 
     @Override
@@ -52,18 +62,17 @@ public class LocalDatabase extends SQLiteOpenHelper {
     }
 
     public void deleteDB(){
-        SQLiteDatabase db = getWritableDatabase();
         db.execSQL("DROP TABLE IF EXISTS " + QUESTION_TABLE);
         db.execSQL("DROP TABLE IF EXISTS " + TRANSLATION_TABLE);
         onCreate(db);
     }
 
-    private void createTables(SQLiteDatabase sqLiteDatabase){
+    private void createTables(){
         String query ="CREATE TABLE " + QUESTION_TABLE + "(" +
                 QUESTION_ID + " INTEGER PRIMARY KEY, " +
                 DIFFICULTY_LEVEL + " INTEGER, " + //from 0 to 2 (easy, medium, hard)
                 RIGHT_ANSWER + " INTEGER"  + ");"; //from 1 to 4
-        sqLiteDatabase.execSQL(query) ;
+        db.execSQL(query) ;
 
         query = "CREATE TABLE " + TRANSLATION_TABLE + "(" +
                 TRANSLATION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -76,23 +85,109 @@ public class LocalDatabase extends SQLiteOpenHelper {
                 ANSWER_3 + " TEXT, " +
                 ANSWER_4 + " TEXT, " +
                 WRONG_ANSWER_COMMENT + " TEXT" + ");";
-        sqLiteDatabase.execSQL(query) ;
+        db.execSQL(query) ;
     }
 
 
-//    public void insertQuestion(QuestionTest question){ //TODO
-//        HashMap<Integer, Object> questionMap = question.getQuestionMap();
-//        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
-//        ContentValues contentValues = new ContentValues();
-//        contentValues.put(RIGHT_ANSWER, (Integer) questionMap.get(QuestionTest.RIGHT_ANSWER_MAP_KEY));
-//        sqLiteDatabase.insert(QUESTION_TABLE, null, contentValues);
-//        contentValues.clear();
-//        contentValues.put(TRANSLATION_ID, );
-//    }
+    public void insertQuestions(HashMap<Integer, Question> questionsMap){
+        try {
+            for(Question question : questionsMap.values()) {
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(QUESTION_ID, question.getQuestionId());
+                contentValues.put(DIFFICULTY_LEVEL, question.getDifficulty());
+                contentValues.put(RIGHT_ANSWER, question.getRightAnswer());
+                db.insert(QUESTION_TABLE, null, contentValues);
+                contentValues.clear();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-//    public void addTranslation(QuestionTest question){ //TODO
-//        HashMap<Integer, Object> translationMap = question.getTranslationMap();
-//
-//    }
+    }
+
+    public void insertTranslations(HashMap<Integer, Translation> translationMap){
+        try {
+            for (Translation translation : translationMap.values()) {
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(QUESTION_ID, translation.getQuestionId());
+                contentValues.put(LANGUAGE_ID, translation.getTranslationId());
+                contentValues.put(TITLE, translation.getTitle());
+                contentValues.put(IMG, translation.getImgLocation());
+                contentValues.put(ANSWER_1, translation.getAnswerOne());
+                contentValues.put(ANSWER_2, translation.getAnswerTwo());
+                contentValues.put(ANSWER_3, translation.getAnswerThree());
+                contentValues.put(ANSWER_4, translation.getAnswerFour());
+                contentValues.put(WRONG_ANSWER_COMMENT, translation.getWrongAnswerComment());
+                db.insert(TRANSLATION_TABLE, null, contentValues);
+//                String query = "UPDATE " + TRANSLATION_TABLE +
+//                        " SET " + QUESTION_ID + " = " + translation.getQuestionId() + ", " +
+//                        LANGUAGE_ID + " = " + translation.getTranslationId() + ", " +
+//                        TITLE + " = " + translation.getTitle() + ", " +
+//                        IMG + " = " + translation.getImgLocation() + ", " +
+//                        ANSWER_1 + " = " + translation.getAnswerOne() + ", " +
+//                        ANSWER_2 + " = " + translation.getAnswerTwo() + ", " +
+//                        ANSWER_3 + " = " + translation.getAnswerThree() + ", " +
+//                        ANSWER_4 + " = " + translation.getAnswerFour() + ", " +
+//                        WRONG_ANSWER_COMMENT + " = " + translation.getWrongAnswerComment() +
+//                        " WHERE " + QUESTION_ID
+//                db.execSQL();
+                contentValues.clear();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<Integer, Question> getQuestions(){
+        HashMap<Integer, Question> questionsMap = new HashMap<>();
+        String selectQuery = "SELECT * FROM " + QUESTION_TABLE
+                + " ORDER BY " + QUESTION_ID + " DESC";
+
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            //Loop through the table rows
+            do {
+                int questionId = cursor.getInt(0);
+                int difficultyLevel = cursor.getInt(1);
+                int rightAnswer = cursor.getInt(2);
+                Question question = new Question(questionId, difficultyLevel, rightAnswer);
+                questionsMap.put(questionId, question);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return questionsMap;
+    }
+
+    public HashMap<Integer, Translation> getTranslations(){
+        HashMap<Integer, Translation> translationsMap = new HashMap<>();
+        String selectQuery = "SELECT * FROM " + TRANSLATION_TABLE
+                + " ORDER BY " + QUESTION_ID + " DESC";
+//        String selectQuery = "SELECT * FROM " + TRANSLATION_TABLE;
+        try {
+            Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            //Loop through the table rows
+            do {
+                int questionId = cursor.getInt(1);
+                String languageId = cursor.getString(2);  //TODO choose language based on phone settings
+                String title = cursor.getString(3);
+                String imgLocation = cursor.getString(4);
+                String answerOne = cursor.getString(5);
+                String answerTwo = cursor.getString(6);
+                String answerThree = cursor.getString(7);
+                String answerFour = cursor.getString(8);
+                String wrongAnswerComment = cursor.getString(9);
+                Translation translation = new Translation(questionId, languageId, title,
+                        imgLocation, answerOne, answerTwo, answerThree, answerFour, wrongAnswerComment);
+                translationsMap.put(questionId, translation);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return translationsMap;
+    }
 
 }
